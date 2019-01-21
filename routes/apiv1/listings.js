d43caa47e52eaab0139020bbd4f0dd17df4c4422
Ferveloper <fermervil@gmail.com>
2019-01-21 23:50:01 +0100

    Basic view and API defined

--- routes/apiv1/listings.js
+++ routes/apiv1/listings.js
@@ -2,55 +2,32 @@
 
 const express = require('express');
 const router = express.Router();
-// const mongoose = require('mongoose');
-// const server = 'localhost';
-// const listingsDB = 'listings_db';
-// const db = require('./lib/connectMongoose')
-// const Listing = require('../../models/Listing')
-
-//Error or success console notifications
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'error de conexion:'));
-// db.once('open', function () {
-//     console.log('Conectado a la base de datos');
-// });
-
-//Connecting to database
-// mongoose.connect(`mongodb://${server}/${listingsDB}`);
+const Listing = require('../../models/Listing')
 
 /* GET home page. */
-// router.get('/listings', function (req, res, next) {
-//     res.locals.title = 'Nodepop';
-//     Listing.find({}, function (err, listings) {
-//         if (err) console.error(err);
-//         res.locals.db = listings;
-//         res.render('index');
-//     })
-// });
-
 router.get('/listings', async (req, res, next) => {
     try {
         // Input values
-        const tag = req - query.tag;
+        const tag = req.query.tag;
         const name = req.query.name;
         const forSale = req.query.sale;
         const price = req.query.price;
         const photo = req.query.photo;
         const skip = parseInt(req.query.skip);
         const limit = parseInt(req.query.limit);
-        // const fields = req.query.fields;
+        const fields = req.query.fields;
         const sort = req.query.sort;
 
         const filter = {};
 
         if (tag) filter.tag = tag;
         if (name) filter.name = name;
-        if (sale) filter.forSale = sale;
-        if (price) filter.name = price;
-        if (photo) filter.name = photo;
+        if (forSale) filter.forSale = sale;
+        if (price) filter.price = price;
+        if (photo) filter.photo = photo;
 
         // buscamos agentes en la base de datos
-        const listings = await Agente.list(filter, skip, limit, fields, sort);
+        const listings = await Listing.list(filter, skip, limit, fields, sort);
 
         res.json({
             success: true,
--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,50 @@
 
 const express = require('express');
 const router = express.Router();
-// const mongoose = require('mongoose');
-// const server = 'localhost';
-// const listingsDB = 'listings_db';
-// require('./apiv1/lib/connectMongoose')
-// const Listing = require('./apiv1/models/Listing')
-
-//Error or success console notifications
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'error de conexion:'));
-// db.once('open', function () {
-//   console.log('Conectado a la base de datos');
-// });
-
-//Connecting to database
-// mongoose.connect(`mongodb://${server}/${listingsDB}`);
+const Listing = require('../models/Listing');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  const Listing = require('../models/Listing')
-  res.locals.title = 'Nodepop';
-  Listing.find({}, function (err, listings) {
-    // let limit = 10;
-    // res.locals.db = pagination(listings, limit);
-    if (err) console.error(err);
-    res.locals.db =listings;
+router.get('/', async (req, res, next) => {
+  // const Listing = require('../models/Listing');
+
+  // Listing.find({}, function (err, listings) {
+  //   if (err) console.error(err);
+  //   res.locals.db = listings;
+  //   res.render('index');
+  // })
+
+  try {
+    // Input values
+    const tag = req.query.tag;
+    const name = req.query.name;
+    const forSale = req.query.sale;
+    const price = req.query.price;
+    const photo = req.query.photo;
+    const skip = parseInt(req.query.skip);
+    const limit = parseInt(req.query.limit);
+    const fields = req.query.fields;
+    const sort = req.query.sort;
+
+    const filter = {};
+
+    if (tag) filter.tag = tag;
+    if (name) filter.name = name;
+    if (forSale) filter.forSale = sale;
+    if (price) filter.price = price;
+    if (photo) filter.photo = photo;
+
+    // buscamos agentes en la base de datos
+    const listings = await Listing.list(filter, skip, limit, fields, sort);
+
+    res.locals.title = 'Nodepop';
+    res.locals.db = listings;
     res.render('index');
-  })
+
+  } catch (err) {
+    next(err);
+    return;
+  }
+
 });
 
 module.exports = router;
