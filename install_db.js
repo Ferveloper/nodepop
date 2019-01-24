0b59092462b7c073be5dc4039842764a9ebf7707
Ferveloper <fermervil@gmail.com>
2019-01-24 22:38:21 +0100

    DB installation script improved User's input for DB delete confirmation and async/await syntax added

--- install_db.js
+++ install_db.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const readline = require('readline');
 const server = 'localhost';
 const listingsDB = 'listings_db';
-const Listing = require('./models/Listing')
+const Listing = require('./models/Listing');
 const fs = require('fs');
 const json = JSON.parse(fs.readFileSync('./listings.json', 'utf8'));
 
@@ -12,28 +13,41 @@ mongoose.connect(`mongodb://${server}/${listingsDB}`, {
     useNewUrlParser: true
 });
 
-//Error and success handlers
 const db = mongoose.connection;
 
+//Error and open handlers
 db.on('error', console.error.bind(console, 'error de conexion:'));
-db.once('open', function () {
-    console.log('Conectado a la base de datos');
-    Listing.deleteMany({}, function (err) {
-        if (err) return console.error(err);
-        console.log('Borrados todos los registros de la base de datos...');
-        Listing.insertMany(json, function (err) {
-            if (err) return console.error(err);
-            console.log('Insertados todos los registros iniciales en la base de datos...')
-            Listing.find(function (err, listings) {
-                if (err) return console.error(err);
-                console.log('Mostrando registros insertados:');
-                console.log(listings);
-                Listing.countDocuments({}, function (err, count) {
-                    console.log(`${count} registros insertados`);
-                    console.log('Cerrando base de datos...');
-                    db.close();
-                });
-            })
-        })
-    });
+db.once('open', async function () {
+    try {
+        console.log('Conectado a la base de datos');
+        const respuesta = await askUser('¿Estás seguro de que quieres borrar TODA la base de datos? (si/no) ');
+
+        if (respuesta.toLowerCase() !== 'si') {
+            console.log('¡Proceso abortado! No se ha borrado la base de datos');
+            process.exit(0);
+        };
+
+        const deleted = await Listing.deleteMany();
+        console.log(`Borrados todos los registros (${deleted.n} documentos en total) de la base de datos...`);
+        const inserted = await Listing.insertMany(json);
+        console.log(`Insertados ${inserted.length} registros iniciales en la base de datos...`);
+        console.log('¡Proceso realizado con éxito!\nCerrando base de datos...');
+        db.close();
+    } catch (err) {
+        console.error(err);
+    };
 });
+
+function askUser(question) {
+    return new Promise((resolve, reject) => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question(question, answer => {
+            rl.close();
+            resolve(answer);
+            return;
+        });
+    });
+};
