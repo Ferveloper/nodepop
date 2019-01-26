6ad9aad2013443773b8a333799d7a35ce826b082
Ferveloper <fermervil@gmail.com>
2019-01-26 17:25:14 +0100

    Cleaning up code

--- models/Listing.js
+++ models/Listing.js
@@ -4,20 +4,20 @@ const mongoose = require('mongoose');
 
 //Defining database schema and model
 const listingSchema = new mongoose.Schema({
-    name: String,
-    forSale: Boolean,
-    price: Number,
-    photo: String,
-    tags: [String]
+  name: String,
+  forSale: Boolean,
+  price: Number,
+  photo: String,
+  tags: [String]
 });
 
 listingSchema.statics.list = (filter, skip, limit, fields, sort) => {
-    const query = Listing.find(filter);
-    query.skip(skip);
-    query.limit(limit);
-    query.select(fields);
-    query.sort(sort);
-    return query.exec();
+  const query = Listing.find(filter);
+  query.skip(skip);
+  query.limit(limit);
+  query.select(fields);
+  query.sort(sort);
+  return query.exec();
 };
 
 const Listing = mongoose.model('Listing', listingSchema);
